8487c93ae0b01d5542a8cf99fcf9e32a580861d0
JasonLinn <cheng11220@gmail.com>
2017-08-15 12:00:21 +0800

    old

---
+++ 11_websocket/readme.md
@@ -0,0 +1,6 @@
+# node.js   example
+
+
+11_websocket
+
+`node chat-server.js`
--- 5_myApp/1_mongoServer.js
+++ 5_myApp/1_mongoServer.js
@@ -1,7 +1,7 @@
 var MongoClient = require('mongodb').MongoClient;
 
 // Connect to the db
-MongoClient.connect("mongodb://localhost:27017/", function (err, db) {
+MongoClient.connect("mongodb://admin:root@ds139082.mlab.com:39082/heroku_fqjf3chf", function (err, db) {
     if (err) throw err;
     //Write databse Insert/Update/Query code here..
     console.log('mongodb is running!');
